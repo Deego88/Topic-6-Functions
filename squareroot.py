70945fbf4c355b96d7f70df592e5e719fabc3094
Richard Deegan <G00387896@gmit.ie>
2020-02-28 18:14:37 +0000

    topic 6 functions add

---
+++ squareroot.py
@@ -0,0 +1,17 @@
+#Ricky Deegan
+# info taken from https://hackernoon.com/calculating-the-square-root-of-a-number-using-the-newton-raphson-method-a-how-to-guide-yr4e32zo
+#This program takes a positive Float as an input
+#and outputs its approx squared root
+
+#create function
+def sqrt (x):
+
+    r = x # approx root
+    percision = 10 ** (-10)
+    while abs (x - r * r) > percision: # reducing the difference in r with percision value
+            r = (r + x / r) / 2 # add approx root with original input / r and result / 2 
+    return r # output 
+
+x = float(input("enter float value you want rooted:")) # input for function 
+
+print(sqrt(x))
